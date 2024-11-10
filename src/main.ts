9037fe46dfb4909d283cb978eb7e4f922f119b48
PavelOreshkin <poreshkin@samokat.ru>
2024-11-10 13:42:40 +0400

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
+import { join } from 'path';
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const document = YAML.load(join(process.cwd(), '/doc/api.yaml'));
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
